b7f2c5c4071cf48346a1571d25a1715c28e6aa13
sch1zo <andreas.eger.reg@googlemail.com>
2010-11-22 20:51:58 +0100

    adminarea spec
    Signed-off-by: sch1zo <andreas.eger.reg@googlemail.com>

---
+++ spec/controllers/Admin/users_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Admin::UsersController do
+  include Devise::TestHelpers
+
+  before do
+    @admin = Factory(:admin)
+    sign_in @admin
+  end
+
+  def mock_user(stubs={})
+    @mock_user ||= mock_model(User, stubs).as_null_object
+  end
+
+  describe "GET index" do
+    it "assigns all users as @users" do
+      User.stub(:all) { [mock_user] }
+      get :index
+      assigns(:users).should eq([mock_user])
+    end
+  end
+
+end
+
